851380f80a0e0bd66f820ef93fd88ace2da6731e
Nadeem J. Qureshi <nadqur@gmail.com>
2013-11-29 22:38:58 +0100

    rails new alim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alim::Application.config.secret_key_base = 'b81f7e1b0b9aa62873bc5ac727779bfea7afcbaf697d7cce14ad36283823a3890f1686d27d28569db36fb636a8f18f6fcafb87efc882818aa41a70f44743c0b0'
